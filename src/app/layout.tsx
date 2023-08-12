93bd1d1a172287fe6e4faa4a83808628b99a8dcb
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-08-12 16:42:36 -0300

    added google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next';
 
-import { Inter } from 'next/font/google';
+import { patrickHandSC } from '@/styles/fonts';
 
 import './globals.css';
-// import { Providers } from '@/redux/providers';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={patrickHandSC.className}>{children}</body>
     </html>
   );
 }
---
+++ src/styles/fonts.ts
@@ -0,0 +1,7 @@
+import { Patrick_Hand_SC } from 'next/font/google';
+
+export const patrickHandSC = Patrick_Hand_SC({
+    style: 'normal',
+    subsets: ['latin'],
+    weight: '400',
+})
