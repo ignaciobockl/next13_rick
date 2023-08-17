7c616b0a80f8165a07605b1f809a94f76236f6bb
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-08-17 10:58:16 -0300

    dpt ok

--- src/components/CSR/DateTimePickerClient.tsx
+++ src/components/CSR/DateTimePickerClient.tsx
@@ -5,29 +5,14 @@ import { useState } from 'react';
 import DateTimePicker, {
   OptionsDateTimePickerProp,
 } from '@/components/Custom Comp/DateTimePicker';
+
 import Button from '../Custom Comp/Button';
-
-/**
- * 1- AAAA-MM-DD: Fecha en formato AAAA-MM-DD.
- * 2- DD/MM/AA o DD/MM/AAAA o D/MM/AA: Fechas en formato DD/MM/AA, DD/MM/AAAA o D/MM/AA, donde D puede ser un solo dígito para el día.
- * 3- T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z: Formato ISO de fecha y hora, opcionalmente con milisegundos y zona horaria.
-* Example: [
-      '2025-01-30',
-      '17/08/2023',
-      '21/8/23',
-      '03-08-23',
-      '2023-08-28T00:00:00.000Z',
-    ]
-*/
-
-const datePattern =
-  /^(\d{4}-\d{2}-\d{2}|\d{1,2}[-/]\d{1,2}[-/]\d{2,4})(T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z)?$/;
 
 const DateTimePickerClient = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   const dateTimePickerOptions: OptionsDateTimePickerProp = {
-    disable: invalidDates.filter((date) => !datePattern.test(date)),
+    // Agregar aca la configuración deseada del dtp.
   };
 
   const handleDateChange = (date: Date) => {
@@ -41,9 +26,11 @@ const DateTimePickerClient = () => {
         className='mb-2'
         onChange={handleDateChange}
         options={dateTimePickerOptions}
+        placeholder=''
         value={selectedDate || undefined}
       />
 
+      {/* // TODO: agregar funcionalidad btn */}
       <Button>Buscar</Button>
     </div>
   );
--- src/components/Custom Comp/DateTimePicker.tsx
+++ src/components/Custom Comp/DateTimePicker.tsx
@@ -1,40 +1,73 @@
 import { FC, useEffect, useRef } from 'react';
+
 import flatpickr from 'flatpickr';
 import { Options } from 'flatpickr/dist/types/options';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
 export type OptionsDateTimePickerProp = Options & {
-  enableTime?: boolean;
+  allowInput?: boolean;
+  clickOpens?: boolean;
   dateFormat?: 'd/m/Y H:i' | 'd/m/Y H:i:S' | 'd/m/Y' | 'H:i' | 'H:i:S';
   defaultDate?: Date;
+  defaultHour?: number;
+  defaultMinute?: number;
+  defaultSeconds?: number;
+  disableMobile?: boolean;
+  enableTime?: boolean;
+  errorHandler?: (e: Error) => void;
   locale?: 'es';
   maxDate?: Date;
   minDate?: Date;
+  mode?: 'single' | 'multiple' | 'range' | 'time';
+  noCalendar?: boolean;
 };
 
-interface DateTimePickerProps {
+export interface DateTimePickerProps {
   className?: string;
   onChange: (selectedDate: Date) => void;
   options?: OptionsDateTimePickerProp;
+  placeholder?: string;
   value?: Date;
 }
 
 const optionsDefaultValues: OptionsDateTimePickerProp = {
-  enableTime: true,
+  enableTime: false,
   dateFormat: 'd/m/Y',
   locale: 'es',
+  mode: 'single',
+  noCalendar: false,
 };
 
 /**
- *
- * @param props
- * @param {} [] -
- * @returns
+ * Component for selecting dates and times with customizable options.
+ * @param {DateTimePickerProps} props - The props for the DateTimePicker component.
+ * @param {string} [props.className = ''] - Additional CSS class for the component.
+ * @param {function} props.onChange - Function to execute when the selected date changes.
+ * @param {OptionsDateTimePickerProp} [props.options] - Configuration options for the DateTimePicker.
+ * @param {boolean} [props.options.allowInput] - Allow manual input of dates.
+ * @param {boolean} [props.options.clickOpens] - Open the calendar when the input is clicked.
+ * @param {string} [props.options.dateFormat='d/m/Y'] - Date and time format to display.
+ *   Possible values: 'd/m/Y H:i' | 'd/m/Y H:i:S' | 'd/m/Y' | 'H:i' | 'H:i:S'
+ * @param {Date} [props.options.defaultDate] - Default date to display.
+ * @param {number} [props.options.defaultHour] - Default hour value.
+ * @param {number} [props.options.defaultMinute] - Default minute value.
+ * @param {number} [props.options.defaultSeconds] - Default seconds value.
+ * @param {boolean} [props.options.disableMobile] - Disable mobile mode.
+ * @param {boolean} [props.options.enableTime] - Enable or disable time selection.
+ * @param {function} [props.options.errorHandler] - Custom error handler function.
+ * @param {string} [props.options.locale] - Local language for the component.
+ * @param {Date} [props.options.maxDate] - Maximum allowed date.
+ * @param {Date} [props.options.minDate] - Minimum allowed date.
+ * @param {string} [props.options.mode='single'] - Calendar selection mode ('single', 'multiple', 'range', 'time').
+ * @param {boolean} [props.options.noCalendar] - Hide the calendar and show only the time picker.
+ * @param {string} [props.options.placeholder] - Placeholder text for the input.
+ * @param {Date} [props.value] - Current value of the DateTimePicker.
+ * @returns {JSX.Element} - The DateTimePicker component.
  */
 
 const DateTimePicker: FC<DateTimePickerProps> = (props) => {
-  const { className = '', onChange, options, value } = props;
+  const { className = '', onChange, options, placeholder, value } = props;
 
   const inputRef = useRef<HTMLInputElement | null>(null);
 
@@ -60,6 +93,7 @@ const DateTimePicker: FC<DateTimePickerProps> = (props) => {
     <input
       className={`p-2 border rounded-md shadow-sm focus:ring focus:ring-indigo-300 ${className}`}
       ref={inputRef}
+      placeholder={placeholder || 'Seleccione la Fecha'}
       type='text'
     />
   );
